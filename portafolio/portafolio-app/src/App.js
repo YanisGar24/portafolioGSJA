7a74610bd762ca3f68d883e51734d018cf4f3ae3
YanisGar24 <janice.a.garciasamperio24@gmail.com>
2024-11-15 14:53:19 -0600

    inicio de proyecto

---
+++ portafolio/portafolio-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Presentacion from './components/presentacion';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Presentacion />
+      
+    </div>
+
+      
+  );
+}
+
+export default App;
---
+++ portafolio/portafolio-app/src/components/presentacion.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export default function Presentacion() {
+    return (
+        <div className="container presentacion">
+            <h1>Esto esta en presentacion</h1>
+            
+
+        </div>
+        
+    );
+  }
+  
