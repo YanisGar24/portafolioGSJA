a8ab352b02636b172180c7a8310d03c41095bf04
YanisGar24 <janice.a.garciasamperio24@gmail.com>
2024-11-15 20:03:55 -0600

    Estructura

--- portafolio/portafolio-app/src/components/presentacion.js
+++ portafolio/portafolio-app/src/components/presentacion.js
@@ -1,14 +1,21 @@
 import React from "react";
-
+import './styles.css';
 
 export default function Presentacion() {
     return (
-        <div className="container presentacion">
-            <h1>Esto esta en presentacion</h1>
+        <div className="container">
             
-
+            <div className="personalInfo">
+                <div className="info texto">
+                    <div className="subInfo">
+                        <h3>Bienvenido a mi portafolio</h3>
+                        <h4>Janice Garcia</h4>
+                    </div>
+                </div>
+                <div className="info imagen">
+                    <img src="http://ximg.es/400x400/000/fff" alt="Imagen de presentación" />
+                </div>
+            </div>
         </div>
-        
     );
-  }
-  
+}
